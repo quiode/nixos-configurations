41d001196909de77f29d4e5713965b06ff503887
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-17 23:04:38 +0200

    steam in bar on pc

--- gaming-pc/home-manager/quio.nix
+++ gaming-pc/home-manager/quio.nix
@@ -1,11 +1,17 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{ inputs
-, lib
-, config
-, pkgs
-, ...
-}: {
+{ ... }:
+{
   # You can import other home-manager modules here
   imports = [ ];
+
+  # find values using "dconf watch /"
+  dconf.settings = {
+    "org/gnome/shell" = {
+      # are at "/run/current-system/sw/share/applications"
+      favorite-apps = [
+        "steam.desktop"
+      ];
+    };
+  };
 }
