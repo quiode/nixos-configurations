0fd73af4161ca880a097a94e37a8390ae402c2c9
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-17 18:22:30 +0200

    change stuff

--- flake.nix
+++ flake.nix
@@ -21,24 +21,6 @@
     in
     {
       # NixOS configuration entrypoint
-      nixosConfigurations.gaming-pc = nixpkgs.lib.nixosSystem {
-        # set system
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs outputs; };
-        # > Our main nixos configuration file <
-        modules = [
-          ./nixos/configuration.nix
-
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.quio = import ./home-manager/quio.nix;
-          }
-        ];
-      };
+      nixosConfigurations.gaming-pc = import ./gaming-pc
     };
 }
