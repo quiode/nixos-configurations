ec58b9ea6704376f90b102c356e643bea22b6f7e
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-21 21:25:11 +0200

    shared, bootloader: plymouth theme

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -11,7 +11,29 @@
 {
   # Bootloader.
   boot = {
+    plymouth = {
+      enable = true;
+      theme = "nixos-bgrt";
+      themePackages = with pkgs; [ nixos-bgrt-plymouth ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+
     loader = {
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      timeout = 0;
+
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
