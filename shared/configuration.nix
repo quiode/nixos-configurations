362dca57a85ca9f2de0c3ed005e0aace2d9c785f
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-26 22:15:22 +0200

    Revert "nix: automatic garbage collection"
    This reverts commit 1d0c03c83cce5fe5f7c6449c41a3e789f74ed552.

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -43,20 +43,14 @@
   ];
 
   # Enable networking
-  networking = let
-    commonPorts = [
-      53317 # localsend
-      51820 # Wireguard - Beaststation
-      51821 # Wireguard - Router
-    ];
-  in {
+  networking = {
     networkmanager.enable = true;
 
     firewall = {
       enable = true;
 
       allowedTCPPorts = [
-        53317 # localsend
+        53317  # localsend
       ];
       allowedUDPPorts = [
         53317 # localsend
@@ -255,7 +249,6 @@
 
   # optimize storage automatically
   nix.optimise.automatic = true;
-  nix.gc.automatic = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
