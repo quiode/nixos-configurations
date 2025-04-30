15a43c2e86a6ca2258ebbd4a200964d369f70a9c
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-04-30 20:15:13 +0200

    flake: clean re-init

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@
   writeShellApplication,
   nh,
 }: let
-  flake_dir = builtins.toString ./.;
+  current_dir = builtins.toString ./.;
 in
   mkShellNoCC {
     name = "Nix Configurations";
@@ -18,14 +18,14 @@ in
       (writeShellApplication {
         name = "update";
         text = ''
-          nix flake update --commit-lock-file --flake ${flake_dir}
+          nix flake update --commit-lock-file --flake ${current_dir}
         '';
       })
 
       (writeShellApplication {
         name = "upgrade";
         text = ''
-          nh os switch ${flake_dir}
+          nh os switch ${current_dir}
         '';
       })
     ];
