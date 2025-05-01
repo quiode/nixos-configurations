0c22d16b011059d88a77de52dbb5287e54480fca
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-05-01 21:02:25 +0200

    laptop: set new keybinds

--- laptop/home-manager/quio.nix
+++ laptop/home-manager/quio.nix
@@ -6,5 +6,31 @@
     "org/gnome/desktop/peripherals/touchpad" = {
       click-method = "areas";
     };
+    # Fixes for ASUS Specific Laptop
+    "org/gnome/shell/keybindings" = {
+      show-screenshot-u = ["<Shift><Super>s"];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/btop" = {
+      name = "Btop";
+      binding = "Launch1";
+      command = "kgx -e btop";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch3" = {
+      name = "TODO 3";
+      binding = "Launch3";
+      command = ''kgx -e "echo TODO: Launch3"'';
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch4" = {
+      name = "TODO 4";
+      binding = "Launch4";
+      command = ''kgx -e "echo TODO: Launch4"'';
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/btop/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch3/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch4/"
+      ];
+    };
   };
 }
