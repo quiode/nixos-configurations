26f833e3713e31310fe88c3dec1543cc8a00e105
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-05-20 18:22:09 +0200

    nvim basics

--- modules/programs/neovim/module.nix
+++ modules/programs/neovim/module.nix
@@ -4,6 +4,7 @@
   pkgs,
   ...
 }: let
+  inherit (pkgs) vimPlugins;
   inherit (lib) genAttrs;
   inherit (lib.types) listOf str;
   inherit (lib.options) mkEnableOption mkOption;
@@ -27,6 +28,40 @@ in {
         vimAlias = true;
         vimdiffAlias = true;
         defaultEditor = true;
+
+        extraLuaConfig = ''
+          vim.o.number = true
+          vim.o.wrap = false
+          vim.o.tabstop = 2
+          vim.o.shiftwidth = 2
+          vim.o.smartcase = true
+          vim.o.ignorecase = true
+          vim.o.hlsearch = false
+          vim.o.signcolumn = 'yes'
+
+          -- Set dracula theme
+          vim.cmd.colorscheme('dracula')
+        '';
+
+        extraPackages = with pkgs; [tree-sitter];
+
+        plugins = with vimPlugins; [
+          dracula-nvim
+          {
+            plugin = nvim-treesitter;
+            config = ''
+              -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+              ensure_installed = "all,
+
+              -- Install parsers synchronously (only applied to `ensure_installed`)
+              sync_install = true,
+
+              -- Automatically install missing parsers when entering buffer
+              -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+              auto_install = true,
+            '';
+          }
+        ];
       };
     });
   };
