554e9c7ec6b5a0fe5a487af60e8faa821f559480
beaststation <beaststation@dominik-schwaiger.ch>
2025-06-04 16:20:33 +0200

    keep scripts

--- hosts/beaststation/compose/down.sh
+++ hosts/beaststation/compose/down.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # Executes compose down for all
-for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose down && cd -); done
-# last shut down traefik
-cd traefik && docker compose down && cd -
+for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose down); done
