de50086b00ef344c4191c0a60abdfffa6b3ec74d
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-06-04 16:13:00 +0200

    update compose scripts

--- hosts/beaststation/compose/down.sh
+++ hosts/beaststation/compose/down.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Executes compose down for all
-for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose down); done
+for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose down && cd -); done
+# last shut down traefik
+cd traefik && docker compose down && cd -
--- hosts/beaststation/compose/up.sh
+++ hosts/beaststation/compose/up.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# First start traefik
+cd traefik && docker compose up -d --remove-orphans && cd -
 # Executes compose up for all
-for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose up -d --remove-orphans); done
+for dir in */; do [ -f "$dir/compose.yml" ] && (cd "$dir" && docker compose up -d --remove-orphans && cd -); done
