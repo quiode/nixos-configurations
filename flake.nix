404e31a1c64a2a16c89ea92d494329879f7c310f
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2025-06-28 14:08:20 +0200

    use nixos 25.05 (oopsie)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Unstable Nixpkgs
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
