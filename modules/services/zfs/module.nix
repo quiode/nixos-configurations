5299a69753431619c77552ab38c03a59fc71796c
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2026-03-21 23:03:42 +0100

    zfs use lts kernel

--- modules/services/zfs/module.nix
+++ modules/services/zfs/module.nix
@@ -18,6 +18,7 @@
           != null
           && (builtins.tryEval kernelPackages).success
           && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+          && (kernelPackages.kernel.isLongTermStable or false)
       )
       linuxKernel.packages;
   in
@@ -45,7 +46,7 @@ in {
         extraPools = cfg.pools;
       };
 
-      # use the latest ZFS-compatible Kernel
+      # use the latest LTS ZFS-compatible Kernel
       # Note this might jump back and forth as kernels are added or removed.
       kernelPackages = latestKernelPackage;
     };
