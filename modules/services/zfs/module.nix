18454692790c966a92a1b8b4a6c1ac8e08e361ae
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2026-03-25 20:47:41 +0100

    zfs: don't set kernel, just pray it works

--- modules/services/zfs/module.nix
+++ modules/services/zfs/module.nix
@@ -4,28 +4,11 @@
   pkgs,
   ...
 }: let
-  inherit (lib) filterAttrs last sort versionOlder;
   inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.modules) mkIf;
   inherit (lib.types) listOf str;
-  inherit (pkgs) linuxKernel zfs-prune-snapshots;
+  inherit (pkgs) zfs-prune-snapshots;
   cfg = config.modules.services.zfs;
-  latestKernelPackage = let
-    zfsCompatibleKernelPackages =
-      filterAttrs (
-        name: kernelPackages:
-          (builtins.match "linux_[0-9]+_[0-9]+" name)
-          != null
-          && (builtins.tryEval kernelPackages).success
-          && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-      )
-      linuxKernel.packages;
-  in
-    last (
-      sort (a: b: (versionOlder a.kernel.version b.kernel.version)) (
-        builtins.attrValues zfsCompatibleKernelPackages
-      )
-    );
 in {
   options.modules.services.zfs.enable = mkEnableOption "enable ZFS";
   options.modules.services.zfs.pools = mkOption {
@@ -44,10 +27,6 @@ in {
       zfs = {
         extraPools = cfg.pools;
       };
-
-      # use the latest ZFS-compatible Kernel
-      # Note this might jump back and forth as kernels are added or removed.
-      kernelPackages = latestKernelPackage;
     };
 
     services = {
